26474335f58ef48e1a032a691aa8eb0f33546414
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2024-10-04 00:20:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -95,14 +95,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Adiciona uma linha em branco
         doc.setFontSize(12);
-        doc.text('Motorista - Loja - Valor Saída - Recebido - Data', 14, 30);
+        doc.text('Motorista - Loja - Valor Saída - Data', 14, 30);
 
         // Adiciona cada saída filtrada
         let y = 40;
         let totalValue = 0; // Para somar os valores das saídas
 
         filteredExpenses.forEach((expense, index) => {
-            const expenseText = `${expense.driver} - ${expense.store} - R$${expense.amount} - R$${expense.received} - ${expense.date}`;
+            const expenseText = `${expense.driver} - ${expense.store} - R$${expense.amount}  - ${expense.date}`;
+            //Para adicionar o lucro e recebido no pdf {expense.profit} - - R$${expense.received}
             doc.text(expenseText, 14, y);
             y += 10;  // Move para a próxima linha
             totalValue += parseFloat(expense.amount); // Acumula o valor total
