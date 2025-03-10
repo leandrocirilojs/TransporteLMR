f00b79c63cc6bcd20d1aa6cdfea7d5c0a67e39e1
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2025-03-10 20:44:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ const generateWhatsAppMessage = () => {
     
     // Adiciona as saídas por dia
     for (const [date, count] of Object.entries(groupedExpenses)) {
-        message += `${dia} / ${mes} ${count} Saída${count > 1 ? 's' : ''}\n`;
+        message += `${date} ${count} Saída${count > 1 ? 's' : ''}\n`;
     }
 
     // Adiciona totais
